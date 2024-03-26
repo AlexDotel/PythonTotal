960ede300d3eb7a78c3d60ca636a4579df4c03ac
Alex <joalredo19@gmail.com>
2024-03-26 11:16:00 +0100

    Avances

--- pythonProject/venv/Dia 9/ElProyectoDia9.py
+++ pythonProject/venv/Dia 9/ElProyectoDia9.py
@@ -1,5 +1,20 @@
-import shutil
+import os
 
-ruta = ('C:\\Users\\joalr\\PycharmProjects\\pythonProject\\venv\\Dia 9\\Proyecto+Dia+9.zip')
-nombre_archivo = 'Proyecto+Dia+9.zip'
-shutil.unpack_archive(nombre_archivo, 'ProyectoDia9', 'zip')
+carpeta_principal = 'C:\\Users\\joalr\\PycharmProjects\\pythonProject\\venv\\Dia 9\\ProyectoDia9\\Mi_Gran_Directorio'
+print(next(os.walk(carpeta_principal)))
+
+def listar_archivos_directorio(directorio):
+    archivos = []
+    for directorio_raiz, directorios, archivos_en_directorio in os.walk(directorio):
+        for archivo in archivos_en_directorio:
+            ruta_completa = os.path.join(directorio_raiz, archivo)
+            archivos.append(ruta_completa)
+    return archivos
+
+directorio_principal = '/ruta/de/tu/directorio/principal'
+archivos_en_directorio = listar_archivos_directorio(carpeta_principal)
+
+for archivo in archivos_en_directorio:
+    print(archivo)
+    lectura = open(archivo, 'r')
+    print(lectura.read())
