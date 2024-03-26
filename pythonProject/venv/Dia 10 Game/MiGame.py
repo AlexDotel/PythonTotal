88e8d1ff2cb6ebb00555fd9e8cbb1a58535d1c6b
Alex Dotel <joalre219@gmail.com>
2024-03-27 00:38:21 +0100

    Inconcluso, no funciona bien la logica del robot, y esta leccion no me es provechosa.

--- pythonProject/venv/Dia 10 Game/MiGame.py
+++ pythonProject/venv/Dia 10 Game/MiGame.py
@@ -1,4 +1,5 @@
 import pygame as p
+import random
 
 # Inicializando pygame
 p.init()
@@ -6,26 +7,53 @@ p.init()
 # Creando pantalla
 pantalla = p.display.set_mode((800, 600))
 
-# Titulo e Icono
+# Titulo, fondo e Icono
 p.display.set_caption('Invasion Espacial')
 icono = p.image.load('ovni.png')
 p.display.set_icon(icono)
+fondo = p.image.load('fondo.jpg')
 
 # Variables Jugador
 rocket_img = p.image.load('rocket.png')
 rocket_x = 368
-rocket_y = 536
+rocket_y = 500
 rocket_x_cambio = 0
 
-# Funcion Jugadore
+# Variables Enemigo
+enemy_img = p.image.load('enemy.png')
+enemy_x = random.randint(0,736)
+enemy_y = random.randint(50,200)
+enemy_x_cambio = 0.3
+enemy_y_cambio = 50
+
+# Variables Bala
+bala_img = p.image.load('bala.png')
+bala_x = 0
+bala_y = 500
+bala_x_cambio = 0
+bala_y_cambio = 1
+bala_visible = False
+
+# Funcion Jugador
 def rocket(x,y):
     pantalla.blit(rocket_img, (x, y))
+
+# Funcion Enemigo
+def enemy(x,y):
+    pantalla.blit(enemy_img, (x, y))
+
+# Funcion Enemigo
+def disparar_bala(x, y):
+    global bala_visible
+    bala_visible = True
+    pantalla.blit(bala_img, (x + 16, x + 10))
 
 # Loop del juego
 se_ejecuta = True
 while se_ejecuta:
-    # Pintando la pantalla
-    pantalla.fill((153,153,255))
+    # Pintando la pantalla o imagen de fondo
+    # pantalla.fill((153,153,255))
+    pantalla.blit(fondo, (0,0))
 
     # Iterar eventos
     for evento in p.event.get():
@@ -34,28 +62,47 @@ while se_ejecuta:
         if evento.type == p.QUIT:
             se_ejecuta = False
 
-        # Evento Presionar Flechas
+        # Evento Presionar Teclas
         if evento.type == p.KEYDOWN:
             if evento.key == p.K_LEFT:
-                rocket_x_cambio = -0.3
+                rocket_x_cambio = -1
             if evento.key == p.K_RIGHT:
-                rocket_x_cambio = 0.3
+                rocket_x_cambio = 1
+            if evento.key == p.K_SPACE:
+                disparar_bala(rocket_x, bala_y)
 
         # Evento Soltar Flechas
         if evento.type == p.KEYUP:
             if evento.key == p.K_LEFT or evento.key == p.K_RIGHT:
                 rocket_x_cambio = 0
 
-    # Modificar ubicacion
+    # Modificar ubicacion rocket
     rocket_x += rocket_x_cambio
 
-    # Limitar movimiento a bordes
+    # Limitar movimiento a bordes rocket
     if rocket_x <= 0:
         rocket_x = 0
     if rocket_x >= 736:
         rocket_x = 736
 
+    # Modificar ubicacion enemy
+    enemy_x += enemy_x_cambio
+
+    # Limitar movimiento a bordes enemy
+    if enemy_x <= 0:
+        enemy_x_cambio = 0.3
+        enemy_y += enemy_y_cambio
+    if enemy_x >= 736:
+        enemy_x_cambio = -0.3
+        enemy_y += enemy_y_cambio
+
+    # Movimento Bala
+    if bala_visible:
+        disparar_bala(rocket_x, bala_y)
+        bala_y = bala_y - bala_y_cambio
+
     rocket(rocket_x, rocket_y)
+    enemy(enemy_x, enemy_y)
 
     # Actualizar
     p.display.update()
