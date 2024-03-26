f9e3dd8701ec9ba2c321dadcf25d3792786fc033
Alex Dotel <joalre219@gmail.com>
2024-03-26 21:46:04 +0100

    Moviendo personaje y limitando bordes

--- pythonProject/venv/Dia 10 Game/MiGame.py
+++ pythonProject/venv/Dia 10 Game/MiGame.py
@@ -11,22 +11,51 @@ p.display.set_caption('Invasion Espacial')
 icono = p.image.load('ovni.png')
 p.display.set_icon(icono)
 
-# Jugador
+# Variables Jugador
 rocket_img = p.image.load('rocket.png')
 rocket_x = 368
 rocket_y = 536
+rocket_x_cambio = 0
 
-def rocket():
-    pantalla.blit(rocket_img, (rocket_x, rocket_y))
+# Funcion Jugadore
+def rocket(x,y):
+    pantalla.blit(rocket_img, (x, y))
 
 # Loop del juego
 se_ejecuta = True
 while se_ejecuta:
     # Pintando la pantalla
-    pantalla.fill((236,236,236))
+    pantalla.fill((153,153,255))
+
+    # Iterar eventos
     for evento in p.event.get():
+
+        # Evento Cerrar Programa
         if evento.type == p.QUIT:
             se_ejecuta = False
-    rocket()
 
+        # Evento Presionar Flechas
+        if evento.type == p.KEYDOWN:
+            if evento.key == p.K_LEFT:
+                rocket_x_cambio = -0.3
+            if evento.key == p.K_RIGHT:
+                rocket_x_cambio = 0.3
+
+        # Evento Soltar Flechas
+        if evento.type == p.KEYUP:
+            if evento.key == p.K_LEFT or evento.key == p.K_RIGHT:
+                rocket_x_cambio = 0
+
+    # Modificar ubicacion
+    rocket_x += rocket_x_cambio
+
+    # Limitar movimiento a bordes
+    if rocket_x <= 0:
+        rocket_x = 0
+    if rocket_x >= 736:
+        rocket_x = 736
+
+    rocket(rocket_x, rocket_y)
+
+    # Actualizar
     p.display.update()
